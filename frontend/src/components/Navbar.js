60396f33493df981f54d850b012ccacc81ee462a
Isaiah Doan <isaiah.doan04@gmail.com>
2025-03-02 20:33:05 -0800

    Adding pages and routing

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,6 +7,15 @@ const Navbar = () => {
                 <Link to="/">
                     <h1>Fresh Connect</h1>
                 </Link>
+                <nav className="nav-links">
+                        <Link to="/shop" className="nav-item">Shop</Link>
+                        <Link to="/sell" className="nav-item">Sell With Us</Link>
+                        <Link to="/about" className="nav-item">About Us</Link>
+                        <Link to="/contact" className="nav-item">Contact</Link>
+                    </nav>
+                    <div className="auth-buttons">
+                        <Link to="/login" className="btn btn-primary">Login / Register</Link>
+                    </div>
             </div>
         </header>
     )
